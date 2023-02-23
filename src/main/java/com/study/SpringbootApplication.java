ec215e6b1d97917e4d61aaee3dbb506cac3aebe1
hubaocheng <admin@example.com>
2023-02-23 09:46:28 +0800

    项目提交

---
+++ src/main/java/com/study/SpringbootApplication.java
@@ -0,0 +1,17 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hbc
+ * @version 1.0
+ * @description 描述
+ * @date 2023/2/22 18:12
+ */
+@SpringBootApplication
+public class SpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class,args);
+    }
+}
