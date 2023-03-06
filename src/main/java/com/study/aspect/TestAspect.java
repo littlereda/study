3334f1fe2b676c4121f26cafc9235821504ffde7
hbc <1761790883@qq.com>
2023-03-06 16:18:22 +0800

    切面拦截器

---
+++ src/main/java/com/study/aspect/TestAspect.java
@@ -0,0 +1,10 @@
+package com.study.aspect;
+
+/**
+ * @author hbc
+ * @version 1.0
+ * @description 描述
+ * @date 2023/3/6 15:28
+ */
+public class TestAspect {
+}
---
+++ src/main/java/com/study/config/WebMvcConfig.java
@@ -0,0 +1,10 @@
+package com.study.config;
+
+/**
+ * @author hbc
+ * @version 1.0
+ * @description 描述
+ * @date 2023/3/6 15:20
+ */
+public class WebMvcConfig {
+}
