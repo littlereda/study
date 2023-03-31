155d2f7950628fddc24448dd19a13fda69453fb8
hbc <1761790883@qq.com>
2023-03-31 10:06:14 +0800

    打包依赖

--- src/main/java/com/study/SpringbootApplication.java
+++ src/main/java/com/study/SpringbootApplication.java
@@ -2,7 +2,6 @@ package com.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author hbc
